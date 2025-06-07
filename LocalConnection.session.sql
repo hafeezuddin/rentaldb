3835ea4edad76d7357de1cc71d6f644d49784c2a
Hafeezuddin <khajahafeezuddin77@gmail.com>
2025-06-06 20:12:22 -0400

    Inactive customers

--- LocalConnection.session.sql
+++ LocalConnection.session.sql
@@ -187,3 +187,11 @@ INNER JOIN rental r ON c.customer_id = r.customer_id
 GROUP BY 1,2
 ORDER BY 3 DESC
 LIMIT 5;
+
+
+/* Customers who have never rented a movie from store */
+SELECT c.customer_id
+FROM customer c
+LEFT JOIN rental r ON c.customer_id = r.customer_id
+WHERE r.customer_id IS NULL
+LIMIT 5;
