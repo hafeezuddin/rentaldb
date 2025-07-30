bce13c53797b4037a433e71657dededb99c6b977
Hafeezuddin Shaik <hafeezskhbablu@gmail.com>
2025-07-29 21:05:30 -0400

    Update rentaldb.sql

--- rentaldb.sql
+++ rentaldb.sql
@@ -421,10 +421,10 @@ SELECT
   CONCAT(c.first_name, ' ', c.last_name) AS customer_name, --Concating firstname and lastname into customername
   c.email, 
   r.rental_date, r.return_date,
-  EXTRACT(DAYS FROM (CURRENT_DATE - r.rental_date)) AS days_rented
+  EXTRACT(DAYS FROM (CURRENT_DATE - r.rental_date)) AS days_rented --No.of days since film was rented out
 FROM customer c
 JOIN rental r ON c.customer_id = r.customer_id
-WHERE r.return_date IS NULL;
+WHERE r.return_date IS NULL; --Filtering customers whose returned date is Null - Unreturned films.
 
 -- =============================================
 -- 11. TEMPORAL TRENDS
