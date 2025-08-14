76255cea4783464679fd1cee90efe9a2b5703d21
Hafeezuddin Shaik <hafeezskhbablu@gmail.com>
2025-08-13 21:02:12 -0400

    Find the top 10 customers who rented the most movies on weekends (Saturday and Sunday)

--- rentaldb.sql
+++ rentaldb.sql
@@ -1027,3 +1027,37 @@ SELECT aar.customer_id, aar.first_name
 FROM above_avg_rentals AS aar
 INNER JOIN below_avg_payment bap ON aar.customer_id = bap.customer_id
 ORDER BY 1;
+
+
+/* Weekend Rental Lovers
+Find the top 10 customers who rented the most movies on weekends (Saturday and Sunday).*/
+--CTE to find top 10 customers who rented most on weekends, corresponding metric (count)
+WITH wknd_rental AS 
+(
+  SELECT
+r.customer_id, c.first_name, COUNT(*) AS wk_rental_count
+FROM rental r
+INNER JOIN customer c ON r.customer_id = c.customer_id
+WHERE EXTRACT(DOW FROM rental_date) IN (0,6) --DOW: Extracting day of week from date 0-6
+GROUP BY 1,2
+ORDER BY count(*) DESC
+LIMIT 10
+),
+--CTE to find total_rentals by the above filtered customers (Top 10 weekend renters) to calculate weekend rental%
+rentals_by_customer_id AS
+(
+  SELECT r2.customer_id, count(*) AS total_rentals
+  FROM rental r2
+  INNER JOIN wknd_rental ON r2.customer_id = wknd_rental.customer_id --INNER JOIN CTE wknd_rentals
+  GROUP BY r2.customer_id
+)
+--Main query to display customer_id, weekend_rentals, total_rentals, percentage of weekend rentals out of total rentals
+SELECT wknd_rental.customer_id,
+wknd_rental.first_name,
+wknd_rental.wk_rental_count, --bigint
+rbci.total_rentals, --bigint
+ROUND((wknd_rental.wk_rental_count::numeric/rbci.total_rentals::numeric)*100,2) AS percentgae_of_rentals_on_weekends
+--Percetage calculation/dt conversion into numeric for percentage calculation
+FROM wknd_rental
+INNER JOIN rentals_by_customer_id rbci ON wknd_rental.customer_id = rbci.customer_id;
+
