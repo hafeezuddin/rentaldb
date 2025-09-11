65985f318b74c1de87a45c6f27f04545e291df1a
Hafeezuddin <hafeezskhbablu@gmail.com>
2025-09-10 21:01:59 -0400

    Top 5 customers in each category spend wise using window function

--- rentaldb.sql
+++ rentaldb.sql
@@ -1307,19 +1307,51 @@ WHERE cwr.total_rentals > 20 AND cwr.city_total > aipc.city_average
 ORDER BY cwr.city_total DESC
 LIMIT 10;                                        
 
-/*Find the top 5 customers (by total spend) in each film category.
-For each category, show: Category name, Customer name (first and last), Total amount they’ve spent on rentals in that category, 
-Number of rentals they made in that category
 
+
+/* Find the top 5 customers (by total spend) in each film category.
+For each category, show:
+Category name, Customer name (first and last)
+Total amount they’ve spent on rentals in that category
+Number of rentals they made in that category
 Only include customers who have rented more than 3 films in that category.
 Order the results by category name, then total spent (descending). */
 
-SELECT c.customer_id, c.first_name, c.last_name, cat.name, COUNT(r.rental_id), SUM(p.amount) FROM customer c
-INNER JOIN rental r ON c.customer_id = r.customer_id
-INNER JOIN payment p ON r.rental_id = p.rental_id
-INNER JOIN inventory i ON r.inventory_id = i.inventory_id
-INNER JOIN film_category fc ON i.film_id = fc.film_id
-INNER JOIN category cat ON fc.category_id = cat.category_id
-GROUP BY 1,2,3,4
-ORDER BY 1;
+--CTE to Dsiplay each customer total_spend in each category
+WITH total_cat_spend AS (
+  Select c.first_name, 
+  c.last_name, 
+  cat.name, 
+  SUM(p.amount) AS total_spent,
+  COUNT(r.rental_id) AS no_of_rentals
+  FROM customer c
+  INNER JOIN rental r ON c.customer_id = r.customer_id
+  INNER JOIN payment p ON r.rental_id = p.rental_id
+  INNER JOIN inventory i ON r.inventory_id = i.inventory_id
+  INNER JOIN film_category fc ON i.film_id = fc.film_id
+  INNER JOIN category cat ON fc.category_id = cat.category_id
+  GROUP BY 1,2,3
+  HAVING COUNT(r.rental_id) > 3 --Filtering customers who rented less than/Equals to 3 films.
+),
+--CTE to rank customers using window function. Partionioning by category and ranking by total_spend in descending order.
+ranked_customers AS (
+SELECT tcs.first_name, 
+tcs.last_name, 
+tcs.name, 
+tcs.total_spent, 
+tcs.no_of_rentals,
+RANK() OVER (PARTITION BY tcs.name ORDER BY tcs.total_spent DESC) AS spend_rank
+FROM total_cat_spend tcs
+)
+--Main query to display top 5 customers in each category.
+SELECT rc.first_name, 
+rc.last_name, 
+rc.name, 
+rc.total_spent, 
+rc.no_of_rentals,
+rc.spend_rank
+FROM ranked_customers rc
+WHERE spend_rank <= 5
+ORDER BY rc.name, rc.total_spent DESC;
+
 
