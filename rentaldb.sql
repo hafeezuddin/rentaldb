5983dbdfa86a2e78cc8ca1a6c9e26b06c95c0676
Hafeezuddin <hafeezskhbablu@gmail.com>
2025-10-22 20:40:40 -0400

    Solution added:Identify customers who consistently rent films every month

--- rentaldb.sql
+++ rentaldb.sql
@@ -13,7 +13,7 @@ WHERE table_name = 'address' AND table_schema = 'public';
 /* Customer data Sample */
 SELECT *
 FROM customer c
-ORDER BY customer_id
+ORDER BY c.customer_id
 LIMIT 5;
 
 
@@ -2122,19 +2122,30 @@ SELECT
   ) AS late_returns_current_month;
 
 
-  /*Business Question: "Identify customers who consistently rent films every month"
+/*Business Question: "Identify customers who consistently rent films every month"
 Requirements:
 Find customers who have rented at least once in every month of the current year (2005)
 Show customer details and their monthly rental consistency */
-
-SELECT 
-  c.customer_id, 
-  c.first_name, 
-  c.last_name, 
-  TO_CHAR(DATE_TRUNC('Month',r.rental_date), 'YYYY-MM') AS month_year,
-  TO_CHAR(DATE_TRUNC('Month',r.rental_date), 'YYYY') AS year,
-  COUNT(*)
+--CTE to select customers who ordered in 2005 (Assuming we are in dec 2005)
+WITH this_year_rentals AS (
+SELECT c.customer_id, c.first_name, c.last_name, r.rental_date,
+EXTRACT('month' FROM r.rental_date) AS month
 FROM customer c
 INNER JOIN rental r ON c.customer_id = r.customer_id
-GROUP BY 1,2,3,4,5
-ORDER BY c.customer_id;
+WHERE r.rental_date BETWEEN '2005-01-01' AND '2005-12-31'
+),
+--CTE to count customers ordering
+every_month AS (
+  SELECT tyr.customer_id, COUNT(DISTINCT tyr.month) AS distinct_months_rented
+  FROM this_year_rentals tyr
+  GROUP BY 1
+  HAVING COUNT(DISTINCT tyr.month) =12
+)
+SELECT em.customer_id, CONCAT(c.first_name,'',c.last_name) AS full_name,
+TO_CHAR(DATE_TRUNC('month', r.rental_date::date), 'YYYY-MM') AS ym,
+COUNT(*) AS rental_counts FROM every_month em 
+INNER JOIN rental r ON em.customer_id = r.customer_id
+INNER JOIN customer c ON r.customer_id = c.customer_id
+GROUP BY 1,2,3
+ORDER BY em.customer_id;
+
